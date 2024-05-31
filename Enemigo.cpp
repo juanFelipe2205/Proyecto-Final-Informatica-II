d42c81b34b6fb17511b9d1f48319399094fca096
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-05-31 18:54:05 -0500

    Update Enemigo.cpp

--- Enemigo.cpp
+++ Enemigo.cpp
@@ -1,9 +1,25 @@
 #include "Enemigo.h"
 
-Enemigo::Enemigo() : vida(3) {
-    // Imagenes del enemigo, aun por implementar
+Enemigo::Enemigo() 
+    : amplitud(50), frecuencia(0.05), fase(0) {
+    setPixmap(QPixmap(":/images/enemigo.png"));
+    timerMovimiento = new QTimer(this);
+    connect(timerMovimiento, &QTimer::timeout, this, &Enemigo::actualizarMovimiento);
+    timerMovimiento->start(16);
 }
 
 void Enemigo::atacar() {
-    // Aun por implementar
+    // Por implementar
+}
+
+void Enemigo::actualizarMovimiento() {
+    setPos(x() + amplitud * qSin(fase), y());
+    fase += frecuencia;
+    if (fase >= 2 * M_PI) {
+        fase = 0;
+    }
+}
+
+void Enemigo::mover() {
+    // Por implementar
 }
