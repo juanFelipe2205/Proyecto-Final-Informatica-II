61777b9b3e502818725be41a9aaadfdc11d3d7e2
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-05-31 18:56:44 -0500

    Update Obstaculo.cpp

--- Obstaculo.cpp
+++ Obstaculo.cpp
@@ -1,10 +1,22 @@
 #include "Obstaculo.h"
 
-Obstaculo::Obstaculo() {
-  //Inicializacion de imagenes de obstaculos, aun en implementacion
+Obstaculo::Obstaculo() 
+    : velocidadY(0), gravedad(0.5) {
+    setPixmap(QPixmap(":/images/obstaculo.png"));
+    timerCaida = new QTimer(this);
+    connect(timerCaida, &QTimer::timeout, this, &Obstaculo::actualizarCaida);
+    timerCaida->start(16);
+}
+
+void Obstaculo::actualizarCaida() {
+    setPos(x(), y() + velocidadY);
+    velocidadY += gravedad;
+    if (y() >= 500) { 
+        setPos(x(), 500);
+        timerCaida->stop();
+    }
 }
 
 void Obstaculo::mover() {
-  //Aun por implementar
-
+    // Por implementar
 }
