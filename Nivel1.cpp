391414d1b167d15709ce7bedd410a5c6f8345a28
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-05-31 18:57:30 -0500

    Update Nivel1.cpp

--- Nivel1.cpp
+++ Nivel1.cpp
@@ -5,11 +5,11 @@ Nivel1::Nivel1(Jugador *jugador) : Nivel(jugador) {}
 void Nivel1::cargar() {
     jugador->setPos(0, 500);  
     Obstaculo *obstaculo1 = new Obstaculo();
-    obstaculo1->setPos(300, 500);
+    obstaculo1->setPos(300, 0); 
     obstaculos.append(obstaculo1);
 
     Obstaculo *obstaculo2 = new Obstaculo();
-    obstaculo2->setPos(600, 500);
+    obstaculo2->setPos(600, 0);  
     obstaculos.append(obstaculo2);
 
     for (Obstaculo *obstaculo : obstaculos) {
