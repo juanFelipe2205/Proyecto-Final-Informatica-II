b559ecdd1ff873f16c02b6505be0d4628706d3ef
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-05-31 18:46:55 -0500

    Create Nivel2.cpp

---
+++ Nivel2.cpp
@@ -0,0 +1,25 @@
+#include "Nivel2.h"
+
+Nivel2::Nivel2(Jugador *jugador) : Nivel(jugador) {}
+
+void Nivel2::cargar() {
+    jugador->setPos(0, 500);  
+    Enemigo *enemigo1 = new Enemigo();
+    enemigo1->setPos(400, 500);
+    enemigos.append(enemigo1);
+
+    Enemigo *enemigo2 = new Enemigo();
+    enemigo2->setPos(800, 500);
+    enemigos.append(enemigo2);
+
+    for (Enemigo *enemigo : enemigos) {
+        jugador->scene()->addItem(enemigo);
+    }
+}
+
+void Nivel2::actualizar() {
+    for (Enemigo *enemigo : enemigos) {
+        enemigo->atacar();
+    }
+    jugador->mover();
+}
