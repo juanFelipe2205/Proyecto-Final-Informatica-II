c9e78b09e7b3d7798fc56499b7447e53857115de
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-02 18:00:57 -0500

    Update Nivel2.h

--- Nivel2.h
+++ Nivel2.h
@@ -2,12 +2,22 @@
 #define NIVEL2_H
 
 #include "Nivel.h"
+#include "Enemigo.h"
 
 class Nivel2 : public Nivel {
+    Q_OBJECT
+
 public:
     Nivel2(Jugador *jugador);
     void cargar() override;
     void actualizar() override;
+
+private:
+    QVector<Enemigo *> enemigos;
+    QTimer *timerGeneracion;
+
+private slots:
+    void generarEnemigo();
 };
 
-#endif 
+#endif
