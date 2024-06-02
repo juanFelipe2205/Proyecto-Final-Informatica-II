21eaf62d6b389c9cd56c8ccc00bf06d20e1af8b2
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-02 17:51:51 -0500

    Update Obstaculo.cpp

--- Obstaculo.cpp
+++ Obstaculo.cpp
@@ -1,30 +1,32 @@
 #include "Obstaculo.h"
 #include <QGraphicsScene>
 
-Obstaculo::Obstaculo(Jugador *jugador) 
-    : velocidadY(0), gravedad(0.5), jugador(jugador) {
+Obstaculo::Obstaculo(Jugador *jugador, bool movimientoHorizontal) 
+    : jugador(jugador), velocidad(5), movimientoHorizontal(movimientoHorizontal) {
     setPixmap(QPixmap(":/images/obstaculo.png"));
-    timerCaida = new QTimer(this);
-    connect(timerCaida, &QTimer::timeout, this, &Obstaculo::actualizarCaida);
-    timerCaida->start(16);
-}
-
-void Obstaculo::actualizarCaida() {
-    setPos(x(), y() + velocidadY);
-    velocidadY += gravedad;
-
-    // Detectar colisión con el jugador
-    if (collidesWithItem(jugador)) {
-        jugador->reducirVida(1);
-        scene()->removeItem(this);
-        delete this;
-        return;
-    if (y() >= 500) {  
-        setPos(x(), 500);
-        timerCaida->stop();
+    timerMovimiento = new QTimer(this);
+    if (movimientoHorizontal) {
+        connect(timerMovimiento, &QTimer::timeout, this, &Obstaculo::mover);
+    } else {
+        connect(timerMovimiento, &QTimer::timeout, this, &Obstaculo::moverCaidaLibre);
     }
+    timerMovimiento->start(16);
 }
 
 void Obstaculo::mover() {
-    //Nada que implementar de momento
+    setPos(x() - velocidad, y());
+
+    if (x() < -50) { 
+        scene()->removeItem(this);
+        delete this;
+    }
+}
+
+void Obstaculo::moverCaidaLibre() {
+    setPos(x(), y() + velocidad);
+
+    if (y() > 600) {  
+        scene()->removeItem(this);
+        delete this;
+    }
 }
