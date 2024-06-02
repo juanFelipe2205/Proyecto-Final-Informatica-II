fa4afbbcef10a983635b3b3e6f648a663b8070f4
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-02 18:19:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 #include "Juego.h"
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-
-    Juego juego;
-    int nivelSeleccionado = 1;  
-    juego.iniciar(nivelSeleccionado);
-
-    return app.exec();
+    QApplication a(argc, argv);
+    Juego *juego = new Juego();
+    juego->show();
+    return a.exec();
 }
