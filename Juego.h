efd44a3fb87dcb8060b5e0dd90cfbd0822c70dc3
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-02 18:11:27 -0500

    Update Juego.h

--- Juego.h
+++ Juego.h
@@ -2,25 +2,29 @@
 #define JUEGO_H
 
 #include <QGraphicsView>
+#include <QWidget>
+#include <QGraphicsScene>
+#include "Jugador.h"
 #include "Nivel.h"
 #include "Nivel1.h"
 #include "Nivel2.h"
 #include "Nivel3.h"
-#include "Jugador.h"
 
 class Juego : public QGraphicsView {
     Q_OBJECT
 
 public:
     Juego(QWidget *parent = nullptr);
-    void iniciar(int nivelSeleccionado);
+    void iniciarJuego();
+    void cambiarNivel(int nivel);
+
+private:
+    QGraphicsScene *scene;
+    Jugador *jugador;
+    Nivel *nivelActual;
 
 private slots:
     void actualizar();
-
-private:
-    Nivel *nivel;
-    Jugador *jugador;
 };
 
-#endif
+#endif 
