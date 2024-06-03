9f20605150b1fb54b483ebd6220d3122e4ebda11
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-03 10:56:19 -0500

    Update Nivel.h

--- Nivel.h
+++ Nivel.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QGraphicsScene>
 #include "Jugador.h"
+#include <QTimer>
 
 class Nivel : public QObject {
     Q_OBJECT
@@ -11,11 +12,13 @@ public:
     explicit Nivel(Jugador *jugador, QObject *parent = nullptr);
     virtual void cargar() = 0;
     virtual void actualizar() = 0;
-    virtual void inicializar();  
-    void detener();  
+    virtual void inicializar();
+    void detener();
 protected:
     Jugador *jugador;
     QGraphicsScene *scene;
+    QTimer *timer;
 };
 
 #endif 
+
