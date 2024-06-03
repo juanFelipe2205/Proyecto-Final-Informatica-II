7cf40ab96ff9edd2dcbbe239e2eeb80d9d777e7e
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-03 10:55:45 -0500

    Update Nivel.cpp

--- Nivel.cpp
+++ Nivel.cpp
@@ -1,38 +1,29 @@
 #include "Nivel.h"
 
-Nivel::Nivel(Jugador *jugador) : jugador(jugador) {}
-
-void Nivel::cargar() {
-    jugador->setPos(100, 500);  
-
-    Obstaculo *obstaculo1 = new Obstaculo();
-    obstaculo1->setPos(300, 500);
-    obstaculos.append(obstaculo1);
-
-    Obstaculo *obstaculo2 = new Obstaculo();
-    obstaculo2->setPos(500, 500);
-    obstaculos.append(obstaculo2);
-
-    Enemigo *enemigo1 = new Enemigo();
-    enemigo1->setPos(700, 500);
-    enemigos.append(enemigo1);
-    for (Obstaculo *obstaculo : obstaculos) {
-        jugador->scene()->addItem(obstaculo);
-    }
-
-    for (Enemigo *enemigo : enemigos) {
-        jugador->scene()->addItem(enemigo);
-    }
+Nivel::Nivel(Jugador *jugador, QObject *parent)
+    : QObject(parent), jugador(jugador) {
+    scene = new QGraphicsScene(this);
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Nivel::actualizar);
 }
 
-void Nivel::actualizar() {
-    for (Obstaculo *obstaculo : obstaculos) {
-        obstaculo->mover();
-    }
+void Nivel::inicializar() {
+    scene->clear();  
+    scene->addItem(jugador);
+    jugador->setPos(400, 500);  
+    jugador->habilitarMovimiento(true);
+    jugador->habilitarSaltoEsquive(false);
+    jugador->setVida(10);  
+    cargar();
+    timer->start(16); 
+}
 
-    for (Enemigo *enemigo : enemigos) {
-        enemigo->atacar();
+void Nivel::detener() {
+    timer->stop();  
+    for (QGraphicsItem *item : scene->items()) {
+        if (item != jugador) {
+            scene->removeItem(item);
+            delete item;
+        }
     }
-
-    jugador->mover();
 }
