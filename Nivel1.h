178388915e717a52092624e564469303122d5a7c
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-03 10:56:59 -0500

    Update Nivel1.h

--- Nivel1.h
+++ Nivel1.h
@@ -2,12 +2,16 @@
 #define NIVEL1_H
 
 #include "Nivel.h"
+#include <QTimer>
 
 class Nivel1 : public Nivel {
+    Q_OBJECT
 public:
-    Nivel1(Jugador *jugador);
+    explicit Nivel1(Jugador *jugador, QObject *parent = nullptr);
     void cargar() override;
     void actualizar() override;
+private:
+    void crearObstaculos();
 };
 
 #endif 
