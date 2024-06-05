2a7b5f33ba7c7eaa67dd551c27f555b4d9eb5218
juanFelipe2205 <153253730+juanFelipe2205@users.noreply.github.com>
2024-06-05 18:50:37 -0500

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "Juego.h"
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    Juego *juego = new Juego();
-    juego->show();
-    return a.exec();
-}
